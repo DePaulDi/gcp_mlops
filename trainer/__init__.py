1edc287c1b6a1706b990e66301ab972f7d72c810
Pavlos <depaulynwa@gmail.com>
2026-04-09 08:20:42 +0300

    clean __init.py

--- trainer/__init__.py
+++
@@ -1,5 +0,0 @@
-# 1. Build the source distribution (creates a dist/ folder)
-python setup.py sdist --formats=gztar
-
-# 2. Upload the created tarball to your GCS bucket
-gcloud storage cp dist/trainer-0.1.tar.gz gs://neuroagro1-gcp-mlops/
